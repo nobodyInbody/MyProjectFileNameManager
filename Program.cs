140d0b021dad850edde60d40907db4db714fc0e0
nobodyoutside <nobodyInbody@outlook.com>
2021-04-23 01:32:28 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ReNameFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            int i = 0;
+            //for문 돌린 부분
+            string 파일 = args[i];
+            FileInfo fi1 = new FileInfo(파일);
+            string path =  fi1.FullName;
+            Console.ReadKey();
+        }
+    }
+}
